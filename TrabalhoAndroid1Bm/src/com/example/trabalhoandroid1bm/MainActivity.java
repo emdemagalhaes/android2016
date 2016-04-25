3825ac666831731490d10f05db3da9d94637fbce
Emerson Magalhaes <emdemagalhaes@gmail.com>
2016-04-25 20:36:24 -0300

    Trabalho de Android

---
+++ TrabalhoAndroid1Bm/src/com/example/trabalhoandroid1bm/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.trabalhoandroid1bm;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button btnEnviar = (Button) findViewById(R.id.btnEnviar);
+
+		btnEnviar.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				setContentView(R.layout.activity_acao);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
